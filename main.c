005d85f7051f3602cc0ee696ed955b3a2907a76c
ile <74355346+ile123@users.noreply.github.com>
2020-12-17 11:19:39 +0100

    Dobro je sve

--- main.c
+++ main.c
@@ -39,13 +39,20 @@ void main()
 		add(dict, buffer);
 	}
 	fclose(fd);
+	//char* niz_stringova[4] = { "Bog","Sirija","i","Bashar" };
+	//int n = duljina_niza(niz_stringova);
+	int n = 100;
+	char** niz_stringova2 = dodavanje_iz_liste_u_niz(dict, n);
+	//dict=dodaj_na_glavu(dict, "Bashar Al-Assad");
+	//dodavanje_na_2_i_3_poziciju(dict);
+	//dodavanje_iz_niza_u_listu(dict, niz_stringova, n);
 	//dodaj_prije_odredenog_elemeata(dict, "a", "KOKO");
 	//dodavanje_prije_odredenog_indeks(dict, 3, "Bubamara");
 	//dict = obrnuta_lista(dict);
 	//brisanje_pod_odredenim_uvjetima(dict);
 	//print(novi_dict);
 	//brisanje_odredenog_elemenata( dict,"Sotona");
-    dodavanje_elementa_nakon_nekog_uvjeta(dict);
+    //dodavanje_elementa_nakon_nekog_uvjeta(dict,"OK");
 	//kopirani_list = create();
     //kopirani_elemnti_nova_lista(kopirani_list,dict, 7);
 	//zamjena_prvi_zdanji(dict);
@@ -56,6 +63,9 @@ void main()
 	printf("\n\n\n");
 	printf("Filtrinaraanranran rjecnik:\n");
 	printf("\n\n\n");
+	for (int i = 0;i < n;i++) {
+		printf("%s\n", niz_stringova2[i]);
+	}
 	//print(nova_lista);
 	//print(dict);
 	//destroy(dict);
