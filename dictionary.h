678e841806bb0a65daf987c01fd03359a5b87b0e
ile <74355346+ile123@users.noreply.github.com>
2020-12-17 11:19:07 +0100

    Zanemrai ovo

--- dictionary.h
+++ dictionary.h
@@ -2,7 +2,7 @@
 #include <ctype.h>
 #include <string.h>
 #include  <stdbool.h> 
-
+#define duljina_niza(x) (sizeof(x)/sizeof(x[0]))
 //zbog nekog razloga ovaj glupi program se buni kada koristim strdup, pa tribam ovo iskljuciti(nesto vezano za c++ se buni)
 #pragma warning(disable : 4996)
 
@@ -28,7 +28,7 @@ void add(Dictionary dict, char* str);
 // ispisuje sve rijeci i broj pojavljivanja svake rijeci
 void print(Dictionary dict);
 
-// briöe cijeli rjeËnik
+// bri≈°e cijeli rje√®nik
 void destroy(Dictionary dict);
 Dictionary filterDictionary(Dictionary indict, int (*filter)(Word* w));
 int filter(Word* w);
@@ -36,7 +36,7 @@ void dodaj_prije_odredenog_elemeata(Dictionary dict, char* trazena_rijec, char* 
 void dodavanje_prije_odredenog_indeks(Dictionary dict, int indeks, char* str);
 Dictionary obrnuta_lista(Dictionary dict);
 void brisanje_pod_odredenim_uvjetima(Dictionary dict);
-void dodavanje_elementa_nakon_nekog_uvjeta(Dictionary dict);
+void dodavanje_elementa_nakon_nekog_uvjeta(Dictionary dict,char* str);
 void kopirani_elemnti_nova_lista(Dictionary kopirani_list,Dictionary dict, int koliko_kopirati);
 void zamjena_prvi_zdanji(Dictionary dict);
 void lista_sa_odrdedenim_rjecima(Dictionary dict,Dictionary nova_lista);
@@ -47,4 +47,7 @@ int broj_rijeci(Dictionary dict);
 void dodaj_na_karj(Dictionary dict, char* str);
 void brisanje_nakraju(Dictionary dict);
 void brisanje_odredenog_elemenata(Dictionary dict, char* element);
+void dodavanje_iz_niza_u_listu(Dictionary dict, char* niz_rijeci, int velicina_niza);
+void dodavanje_na_2_i_3_poziciju(Dictionary dict);
+char** dodavanje_iz_liste_u_niz(Dictionary dict, int n);
 #endif
